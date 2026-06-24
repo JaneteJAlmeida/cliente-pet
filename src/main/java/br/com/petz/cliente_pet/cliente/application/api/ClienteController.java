dcddd4e55767b5c784b5a7c14012dccca3dcc170
JaneteJAlmeida <janebeatriz26@yahoo.com.br>
2026-06-24 15:35:57 -0300

    implementa Controller e fluxo para service

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteController.java
@@ -1,16 +1,24 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
+import br.com.petz.cliente_pet.cliente.application.service.ClienteService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Log4j2
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class ClienteController implements ClienteAPI {
+    private final ClienteService clienteService;
 
     @Override
     public ClienteResponse postCliente(ClienteRequest clienteRequeste) {
         log.info("[inicia] ClienteController - postCliente");
+        ClienteResponse clienteCriado = clienteService.criaCliente(clienteRequeste);
         log.info("[finaliza] ClienteController - postCliente");
-        return null;
+        return clienteCriado;
     }
 }
