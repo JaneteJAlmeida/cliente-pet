eb921e70944bf9d124caff1086744b179f06c846
JaneteJAlmeida <janebeatriz26@yahoo.com.br>
2026-06-28 10:52:51 -0300

    modifica cliente para nao permitir email e cpf duplicados

--- src/main/java/br/com/petz/cliente_pet/cliente/domain/Cliente.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/domain/Cliente.java
@@ -28,6 +28,7 @@ public class Cliente {
     private String nomeCompleto;
     @NotBlank
     @Email
+    @Column(unique = true)
     private String email;
     @NotBlank
     private String celular;
@@ -37,6 +38,7 @@ public class Cliente {
     @NotNull
     private LocalDate dataNascimento;
     @CPF
+    @Column(unique = true)
     private String cpf;
     @NotNull
     private Boolean aceitaTermos;
