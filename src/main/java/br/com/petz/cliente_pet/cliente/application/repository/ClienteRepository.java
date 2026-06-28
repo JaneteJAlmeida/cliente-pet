72374a6a4cbf37bf823f5dc33837bf769c377820
JaneteJAlmeida <janebeatriz26@yahoo.com.br>
2026-06-28 10:44:04 -0300

    implenta delete

--- src/main/java/br/com/petz/cliente_pet/cliente/application/repository/ClienteRepository.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/repository/ClienteRepository.java
@@ -9,6 +9,6 @@ import java.util.UUID;
 public interface ClienteRepository {
     Cliente salva(Cliente cliente);
     List<Cliente> buscaTodosClientes();
-
     Cliente buscaClienteAtravesId(UUID idCliente);
+    void deletaCliente(Cliente cliente);
 }
