cee25225e40170abc15855a278d45b38998481d3
JaneteJAlmeida <janebeatriz26@yahoo.com.br>
2026-06-30 21:40:50 -0300

    implementa fluxo da api

---
+++ src/main/java/br/com/petz/cliente_pet/pet/application/api/PetAPI.java
@@ -0,0 +1,16 @@
+package br.com.petz.cliente_pet.pet.application.api;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/v1/cliente/{idCliente}/pet")
+public interface PetAPI {
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    PetResponse postCliente(@PathVariable UUID idCliente,
+                            @RequestBody @Valid PetRequest petRequest);
+}
